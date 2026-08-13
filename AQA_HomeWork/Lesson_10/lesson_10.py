32f7104d1ca936cf41dd1d430f4b3f8e38d38258
Eduard Turukhanov <edoo1995@gmail.com>
2026-08-13 16:15:27 +0700

    lesson_10

--- AQA_HomeWork/Lesson_10/lesson_10.py
+++ AQA_HomeWork/Lesson_10/lesson_10.py
@@ -1,7 +1,7 @@
 # ЗАДАНИЕ 1: Распаковка списка и слияние
 statuses = ["queued", "running", "testing", "deploy", "done"]
-first,*middle,last = statuses
-new_statuses = [*middle,*["failed", "skipped"]]
+first, *middle, last = statuses
+new_statuses = [*middle, *["failed", "skipped"]]
 print(first)
 print(new_statuses)
 print(last)
@@ -11,9 +11,11 @@ print(last)
 # Дано:
 browser = {"browser": "chrome", "timeout": 3000}
 options = {"headless": True, "timeout": 5000}
-#
+
+
 def start_session(browser, timeout, headless):
-     return print(f"{browser}, timeout={timeout}, headless={headless}")
+    return print(f"{browser}, timeout={timeout}, headless={headless}")
+
 
 config = {**browser, **options}
 start_session(**config)
