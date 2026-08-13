20cc803193740c49b5155c7c5ecd02fe2eefe13d
Eduard Turukhanov <edoo1995@gmail.com>
2026-08-13 16:07:19 +0700

    lesson_10

---
+++ AQA_HomeWork/Lesson_10/lesson_10.py
@@ -0,0 +1,20 @@
+# ЗАДАНИЕ 1: Распаковка списка и слияние
+statuses = ["queued", "running", "testing", "deploy", "done"]
+first,*middle,last = statuses
+new_statuses = [*middle,*["failed", "skipped"]]
+print(first)
+print(new_statuses)
+print(last)
+
+# ЗАДАНИЕ 2: Словарь, слияние и вызов функции
+
+# Дано:
+browser = {"browser": "chrome", "timeout": 3000}
+options = {"headless": True, "timeout": 5000}
+#
+def start_session(browser, timeout, headless):
+     return print(f"{browser}, timeout={timeout}, headless={headless}")
+
+config = {**browser, **options}
+start_session(**config)
+print(config)
